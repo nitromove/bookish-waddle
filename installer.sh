3dfb518cb20676e5193612c29870f5d7c56a6330
Ishaq Suleiman <68830069+nitromove@users.noreply.github.com>
2024-03-21 10:11:54 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,65 +1,93 @@
-# Install kops
-sudo wget https://github.com/kubernetes/kops/releases/download/v1.23.0/kops-linux-amd64
-sudo chmod +x kops-linux-amd64
-sudo mv kops-linux-amd64 /usr/local/bin/kops
-
-# Install hub
-sudo apt-get install snapd
-sudo snap install hub --classic
-
-# Install Jenkins X (jx)
-curl -L "https://github.com/jenkins-x/jx/releases/download/$(curl --silent "https://github.com/jenkins-x/jx/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#')/jx-linux-amd64.tar.gz" | tar xzv "jx"
-chmod +x jx
-sudo mv jx /usr/local/bin
-
-# Update the ubuntu server
+# update the ubuntu server
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Install unzip
+
+# install unzip
+
 sudo apt-get install unzip
 
-# Install terraform
+# install terraform
+
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
 wget -O- https://apt.releases.hashicorp.com/gpg | \
 gpg --dearmor | \
 sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
 gpg --no-default-keyring \
 --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
 --fingerprint
+
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
+
 sudo apt-get update && sudo apt-get install terraform
 
-# Install kubectl
+# install kubectl
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 chmod +x kubectl 
 mkdir -p ~/.local/bin
 mv ./kubectl ~/.local/bin/kubectl
 
-# Install aws cli
+
+# install aws cli
+
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
 unzip awscliv2.zip
+
 sudo ./aws/install --update
 
-# Install helm
+# install helm
+
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+
 sudo apt-get install apt-transport-https --yes
+
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
 sudo apt-get update
+
 sudo apt-get install helm
 
-# Install Prometheus using Helm
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/prometheus
 
-# Firewall configurations
+# install jenkins
+
+sudo apt install fontconfig openjdk-17-jre -y
+
+java -version
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update -y
+
+sudo apt-get install jenkins -y
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
 sudo ufw allow OpenSSH 
+
 sudo ufw enable
+
 sudo ufw allow 8080
+
 sudo ufw status
